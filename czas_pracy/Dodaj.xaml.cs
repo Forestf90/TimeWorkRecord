6e3b23acff98b1f60446bcbc8fadfb741a1ee691
Forestf90 <michalsliwa96@wp.pl>
2018-04-24 21:20:07 +0200

    Dapper

--- czas_pracy/Dodaj.xaml.cs
+++ czas_pracy/Dodaj.xaml.cs
@@ -71,11 +71,25 @@ namespace czas_pracy
                     try
                     {
                         pesel = ZPesel.Text;
-                        string selekcikZ = @"Select ID_Pracownik from Pracownik Where pesel='" + pesel + "'";
+                        string selekcikZ = @"Select ID_Pracownik , TypUmowy from Pracownik Where pesel='" + pesel + "'";
                         SqlCommand SZ = new SqlCommand(selekcikZ, con);
 
-                        int result = (int)SZ.ExecuteScalar();
-                       
+                        int result=0;// = (int)SZ.ExecuteScalar();
+                            string typU="";
+
+                        using (SZ)
+                        {
+                            SqlDataReader reader = SZ.ExecuteReader();
+                            while (reader.Read())
+                            {
+                                result = reader.GetInt32(0);
+                                typU = reader.GetString(1);
+
+                            }
+                            reader.Close();
+                        }
+
+                        if (typU != "o prace   ") { MessageBox.Show("Dany pracownik pracuje na umowe zlecenie - nie przysługuja mu żadne zwolnienia"); break; }
 
                         string Zdzien = Zdd.Text;
                         string Zmiesiac =Zmm.Text;
@@ -121,20 +135,123 @@ namespace czas_pracy
 
                 case 2:
 
-                    //zmiana zmienych z zwolnienia na rozliczenie
-                    //try
-                    //{
-                    //    pesel = ZPesel.Text;
-                    //    string selekcikZ = @"Select ID_Pracownik from Pracownik Where pesel='" + pesel + "'";
-                    //    SqlCommand SZ = new SqlCommand(selekcikZ, con);
 
-                    //    int result = (int)SZ.ExecuteScalar();
-                    //    MessageBox.Show(Convert.ToString(result));
-                    //}
-                    //catch
-                    //{
-                    //    MessageBox.Show("Nie znaleziono pracownika z takim peselem");
-                    //}
+                    try
+                    {
+                        pesel = RPesel.Text;
+                        string selekcikR = @"Select ID_Pracownik , TypUmowy, Wynagrodzenie , CzasPracy from Pracownik Where pesel='" + pesel + "'";
+                        SqlCommand SR = new SqlCommand(selekcikR, con);
+
+                        int Rresult =0;
+                        string TypUmowy = "";
+                        decimal ile_na_godzine=0;
+                        int ile_h_na_dzien=0;
+                        using (SR)
+                        {
+                            SqlDataReader reader =SR.ExecuteReader();
+                            while (reader.Read())
+                            {
+                               Rresult = reader.GetInt32(0);
+                               TypUmowy = reader.GetString(1);
+                               ile_na_godzine = reader.GetDecimal(2);
+                              ile_h_na_dzien = reader.GetInt32(3);
+                            }
+                        reader.Close();
+                        }
+                       
+
+
+                        string Rmiesiac = Rmm.Text;
+                        
+                        string Rrok = Rrrrr.Text;
+                    int godziny_przepracowane = Convert.ToInt32(RGodzinyPrzepracowane.Text);
+
+                        string selekcikData = @"Select ID_Rozliczenie from Rozliczenie Where ID_Pracownik='" + Rresult + "' AND" +
+                            " Miesiac=" + "'" + Rrok + "." + Rmiesiac + ".1'";
+                        SqlCommand Rsprawdzdate = new SqlCommand(selekcikData, con);
+
+                        var sprawdzdate = Rsprawdzdate.ExecuteScalar();
+                        if (sprawdzdate != null) MessageBox.Show("Ten pracownik posiada juz rozliczenie za ten miesiac");
+                        else
+                        {
+                            //MessageBox.Show(selekcikData);
+                            string godziny = RGodzinyPrzepracowane.Text;
+                            decimal premia = Convert.ToDecimal(RPremia.Text);
+
+                            DateTime normalny = new DateTime(Convert.ToInt32(Rrok), Convert.ToInt32(Rmiesiac), 1);
+                            DateTime normalny_koniec=new DateTime(Convert.ToInt32(Rrok), Convert.ToInt32(Rmiesiac), 
+                                DateTime.DaysInMonth(Convert.ToInt32(Rrok), Convert.ToInt32(Rmiesiac)));
+
+                            double ile_dni_roboczych=GetBusinessDays(normalny, normalny_koniec);
+                            double ile_dni_urlopu=0;
+                            decimal PlatnoscZaUrlop=0;
+
+
+
+                            int dlugoscUrlopu=0;
+                            DateTime Urlop_start = new DateTime();
+                            decimal platnoscUrlopu=0;
+                        int id_zwolnienia = new int();
+                            using (SqlCommand command = new SqlCommand("select DataRozpoczecia , dlugosc , platnosc ,ID_Zwolnienie from Zwolnienie "+
+                                        "Where DataRozpoczecia  Like '"+Rrok+"-"+Rmiesiac+"-__'"+"AND ID_Pracownik='"+Rresult+"'", con))
+                            {
+                                SqlDataReader reader = command.ExecuteReader();
+                                while (reader.Read())
+                                {
+                                    dlugoscUrlopu = reader.GetInt32(1);    
+                                    Urlop_start = reader.GetDateTime(0);
+                                    platnoscUrlopu = reader.GetDecimal(2);
+                                id_zwolnienia= reader.GetInt32(3);
+                            }
+                            reader.Close();
+                            }
+
+                            if (dlugoscUrlopu == 0) MessageBox.Show("jest null");
+                            else
+                            {
+                                
+                                DateTime Urlop_koniec = new DateTime();
+                                Urlop_koniec = Urlop_start;
+                                Urlop_koniec=Urlop_koniec.AddDays(dlugoscUrlopu);
+                                ile_dni_urlopu = GetBusinessDays(Urlop_start, Urlop_koniec);
+                                
+                                PlatnoscZaUrlop = (int)ile_dni_urlopu * platnoscUrlopu *ile_na_godzine * ile_h_na_dzien; // Co ???
+                                MessageBox.Show(Convert.ToString(PlatnoscZaUrlop));
+                            }
+
+                        double dni_przepracowane = ile_dni_roboczych - ile_dni_urlopu;
+                        decimal brutto;
+                        if (TypUmowy == "o prace   ") brutto = (godziny_przepracowane * ile_na_godzine) + PlatnoscZaUrlop + premia;
+                        else brutto = ile_na_godzine + premia;
+
+
+                        string premia_z_kropka = Convert.ToString(premia);
+                        premia_z_kropka = premia_z_kropka.Replace(",", ".");
+                        string brutto_kropka = Convert.ToString(brutto);
+                        brutto_kropka = brutto_kropka.Replace(",", ".");
+
+
+                        string insercikR = @"INSERT INTO Rozliczenie VALUES(" + "'" + Rresult + "' ,'" +id_zwolnienia + "' ,'"
+                       + Rmiesiac + ".01." + Rrok + "' ," + "'" + godziny_przepracowane + "'," + "'" + premia_z_kropka + "'," + "'" + brutto_kropka + "')";
+
+
+                        SqlCommand DR = new SqlCommand(insercikR, con);
+                        DR.ExecuteNonQuery();
+                         if(TypUmowy !="o prace   ")MessageBox.Show("Pracownik pracuje na umowe zlecenie wiec jego przepracowane godziny nie są brane pod uwage");
+                            MessageBox.Show("Rozliczenie zostało dodane!");
+
+                            RPesel.Text = ""; Rmm.Text = ""; Rrrrr.Text=""; RGodzinyPrzepracowane.Text = ""; RPremia.Text = "";
+
+                    }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Nie znaleziono pracownika z takim peselem");
+                    }
+
+
+
+
 
 
                     break;
@@ -143,6 +260,18 @@ namespace czas_pracy
             }
 
 
+        }
+
+        public static double GetBusinessDays(DateTime startD, DateTime endD)
+        {
+            double calcBusinessDays =
+                1 + ((endD - startD).TotalDays * 5 -
+                (startD.DayOfWeek - endD.DayOfWeek) * 2) / 7;
+
+            if (endD.DayOfWeek == DayOfWeek.Saturday) calcBusinessDays--;
+            if (startD.DayOfWeek == DayOfWeek.Sunday) calcBusinessDays--;
+
+            return calcBusinessDays;
         }
 
         private void Anuluj_Click(object sender, RoutedEventArgs e)
--- czas_pracy/Raport.xaml.cs
+++ czas_pracy/Raport.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using Dapper;
+
 
 namespace czas_pracy
 {
@@ -19,9 +22,32 @@ namespace czas_pracy
     /// </summary>
     public partial class Raport : Window
     {
+        SqlConnection con;
+     
         public Raport()
         {
+        
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            con = new SqlConnection(@"Data Source=DESKTOP-0PBRBDG;Initial Catalog=Dolars;Integrated Security=True");
+            con.Open();
+
+            dataGridView.ItemsSource = PrintData();
+        }
+
+
+        public List <Pracownik> PrintData()
+        {
+            string querry = @"SELECT * FROM Pracownik";
+
+            var wynikiList = con.Query<Pracownik>(querry).ToList();
+
+            return wynikiList;
+
+        }
+
     }
 }
